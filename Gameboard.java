b93311f9fa3d8c12a88a35cf0b8fec6931839007
hongys97 <hongys97@gmail.com>
2017-06-24 07:31:48 -0400

    Add files via upload

--- Gameboard.java
+++ Gameboard.java
@@ -1,26 +1,24 @@
-import objectdraw.*;
+import java.swing.JFrame;
 
 public class Gameboard{
+    
 
-  private Line[] lineArray;
-
-  // we create the visual board within the constructor
-  //createBoard(size, size);
-
-
-
-  // the method that sets up the visual board
-  public void createBoard(int length, int height){
-
-    // start creating the vertical lines
-    for (int x = 0; x <= length; x++){
-      Line vert = new Line(x, 0, x, height);
-      //lineArray[x] = vert;
+    // we create the visual board within the constructor 
+    public Gameboard(int length, int height){
+        
+        JFrame window = new JFrame;
+        window.setSize(length, height);
+        window.setTitle("Omok Game");
+        
+        // loops that create the lines that make up the board 
+        for (int x = 0; x <= length; x++){
+            for (int y = 0; y <= height; y++){
+                
+                // create lines
+                window.drawLine(x, 0, x, height);
+                window.drawLine(0, y, length, y);
+            }
+        }
     }
-
-    for (int y = 0; y <= height; y++){
-      Line horiz = new Line(0, y, length, y);
-      //lineArray
-    }
-  }
 }
+                
